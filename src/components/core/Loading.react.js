7821a6f479f179a016175b933e2545645350cac1
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-12-15 18:46:07 +0100

    Changed Loading by our custom loading React component

--- src/components/core/Loading.react.js
+++ src/components/core/Loading.react.js
@@ -1,11 +1,21 @@
 import {connect} from 'react-redux'
 import {any} from 'ramda'
 import React, {PropTypes} from 'react'
+import '!style!css!./loading.css'; // eslint-disable-line
 
 function Loading(props) {
     if (any(r => r.status === 'loading', props.requestQueue)) {
         return (
-            <div className="_dash-loading-callback"/>
+            <div className="dialog-component">
+                <div className="cover"></div>
+                <div className="dialog">
+                    <p>Loading...</p>
+
+                    <div className="loader"></div>
+
+                    <p>Please wait.</p>
+                </div>
+            </div>
         );
     } else {
         return null;
